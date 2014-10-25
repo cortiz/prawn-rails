26b69c4a0f9bc804997c38b46237b68b5ed0581e
Nando Sousa <nandosousafr@gmail.com>
2014-10-25 16:24:31 -0300

    fixed default config load

--- lib/prawn-rails/document.rb
+++ lib/prawn-rails/document.rb
@@ -5,6 +5,11 @@ module PrawnRails
   # This derives from Prawn::Document in order to override defaults. Note
   # that the Prawn::Document behaviour itself shouldn't be changed.
   class Document < Prawn::Document
+    def initialize(opts = {})
+      default = PrawnRails.config.to_h.merge(opts)
+      super(default)
+    end
+
     # Typicall text expects a string. But rails views have this interesting
     # concept that they implicitly call `to_s` on all the variables before
     # rendering. So, passing an integer to text fails:
