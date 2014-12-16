6cf9010d6f766b09637f8533671462038e73f48d
Chris Gunther <chris@room118solutions.com>
2014-12-16 18:26:52 -0500

    Allow the filename of the PDF to be configured
    Setting @filename in the action allows you to specify the name the file
    should be downloaded as, should a use try to save the PDF.

--- lib/prawn-rails/renderer.rb
+++ lib/prawn-rails/renderer.rb
@@ -3,7 +3,12 @@ require "prawn-rails/document"
 module PrawnRails
   class Renderer
     def self.call(template)
-      template.source.strip
+      %{
+        @filename ||= "\#{controller.action_name}.pdf"
+        controller.response.headers['Content-Disposition'] = "inline; filename=\\\"\#{@filename}\\\""
+
+        #{template.source.strip}
+      }
     end
   end
 end
