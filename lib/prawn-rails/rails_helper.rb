da56c517c1700460b15a4434f734bc640de7e657
Weston Ganger <westonganger@gmail.com>
2017-01-17 15:47:28 -0800

    Set PDF title metadata to @filename (#24)
    The browser tab title is taken from the PDF documents Title metadata attribute. This now sets the Title attribute of the metadata based on the @filename without the file extension.

--- lib/prawn-rails/rails_helper.rb
+++ lib/prawn-rails/rails_helper.rb
@@ -4,7 +4,10 @@ module PrawnRails
   module RailsHelper
     def prawn_document(options={})
       options.reverse_merge!(:page_layout => PrawnRails.config.page_layout,
-                             :page_size => PrawnRails.config.page_size)
+                             :page_size => PrawnRails.config.page_size,
+                             :info => {
+                               :Title => @filename.sub(/\.(p|P)(d|D)(f|F)$/, '')
+                             })
 
       options.reverse_merge!(:skip_page_creation => true) if PrawnRails.config.skip_page_creation
 
