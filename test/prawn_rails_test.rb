50e15989b9f074c4d92b0f44ca13be63f194b23a
Chris Gaffney <gaffneyc@gmail.com>
2018-06-27 16:24:14 -0400

    Adds test for rendering text of non-strings
    This was added in 8cbea49b as a quality of life improvement and to
    better match Rails's behavior for erb templates (e.g. <%= %> calls to_s).
    
    I'm adding this test as a reminder of why the PrawnRails::Document class
    exists: to provide an API adapter to Prawn::Document.

--- test/prawn_rails_test.rb
+++ test/prawn_rails_test.rb
@@ -1,11 +1,23 @@
 ### FOR GENERIC NON-RAILS TESTS
 
 require 'test_helper'
+require 'pdf/reader'
 
 class PrawnRailsTest < ActiveSupport::TestCase
+  include PrawnRails::RailsHelper
 
-  test "config" do
-    assert true
+  test "text can take non strings" do
+    @filename = ""
+
+    output = prawn_document do |pdf|
+      pdf.text "Number:"
+      pdf.text 10
+    end
+
+    reader = PDF::Reader.new(StringIO.new(output))
+    assert_not_nil(reader)
+
+    page = reader.page(1).to_s
+    assert page.include?("Number:\n10")
   end
-
 end
